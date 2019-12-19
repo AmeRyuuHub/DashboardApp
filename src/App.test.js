259104f671c40743d689a7cfa1cffd9e08758c34
Ame <malikov.stanislav@gmail.com>
2019-12-19 13:28:15 +0200

    try to fix app test #1

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose,applyMiddleware } from 'redux';
+import rootReducer from './store/redusers';
+import { createBrowserHistory } from 'history';
+import { thunkMiddleware } from 'redux-thunk';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunkMiddleware)));
+const history = createBrowserHistory();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render( <Router history={history}>
+    <Provider store={store}>
+        <App />
+    </Provider>
+</Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- src/components/MainBar/components/SubMenu.jsx
+++ src/components/MainBar/components/SubMenu.jsx
@@ -6,23 +6,25 @@ import {Link} from 'react-router-dom';
 import { MenuItem, Menu, Button, Icon, Avatar} from '@material-ui/core';
 
 
-
 const SubMenu = props => {
-
- 
-
-  return (
-    
-(props.auth && props.user) ? 
-
-<Button  component={Link} to="/profile"
-        color="inherit" endIcon={ <Avatar> <Icon component={dataUsersRoleIcons[props.user.icon]} /> </Avatar>}> {props.user && props.user.name}</Button>
-     
-  : 
-  
-  
-  <Button variant="outlined" color="inherit" component={Link} to="/auth"> Log In</Button>
-      
+  return props.auth && props.user ? (
+    <Button
+      component={Link}
+      to="/profile"
+      color="inherit"
+      endIcon={
+        <Avatar>
+         
+          <Icon component={dataUsersRoleIcons[props.user.icon]} />{" "}
+        </Avatar>
+      }
+    >
+      {props.user && props.user.name}
+    </Button>
+  ) : (
+    <Button variant="outlined" color="inherit" component={Link} to="/auth">
+      Log In
+    </Button>
   );
 };
 
